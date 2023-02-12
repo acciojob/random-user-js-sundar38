db8321916991e183f07fb984d6bc32267b330366
Penugonda <sundara.k.penugonda@accenture.com>
2023-02-13 00:04:13 +0530

    first

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your code here
+let agevalue=document.getElementById("age")
+let mailvalue=document.getElementById("email")
+let phonevalue=document.getElementById("phone")
+let printvalue=document.getElementById("additionalinfo")
+
+fetch("https://randomuser.me/api/")
+.then(data=> data.json())
+.then(data=> buttonsprint(data.results[0]))
+
+function buttonsprint(data){
+    console.log(data);
+    agevalue.addEventListener("click", ()=>{
+        let printage=`
+        <div>
+    
+       ${data.dob.age}
+        </div>
+        `
+        printvalue.innerHTML=printage
+    })
+    
+}
+
